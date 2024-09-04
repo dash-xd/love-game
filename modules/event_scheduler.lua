4f9ae6e998ca7b7f4e950451a90ef34578903277
dev@dashxd <125025267+dash-xd@users.noreply.github.com>
2024-09-03 22:15:36 -0700

    Create event_scheduler.lua

---
+++ modules/event_scheduler.lua
@@ -0,0 +1,38 @@
+local select = require('posix.sys.select')
+local unistd = require('posix.unistd')
+local fcntl = require('posix.fcntl')
+
+local EventScheduler = {}
+EventScheduler.__index = EventScheduler
+
+function EventScheduler.new()
+    local self = setmetatable({}, EventScheduler)
+    self.coroutines = {}  -- Map of file descriptor to coroutine
+    self.read_fds = {}    -- List of file descriptors to monitor for readability
+    return self
+end
+
+-- Register a coroutine to wait on a socket for readability
+function EventScheduler:wait_for_read(socket_fd, co)
+    self.coroutines[socket_fd] = co
+    table.insert(self.read_fds, socket_fd)
+end
+
+-- Run the scheduler and resume coroutines when events occur
+function EventScheduler:run()
+    while true do
+        -- Use select to monitor the read_fds for readability
+        local ready_fds = select.select(self.read_fds, nil, nil, 1)  -- 1-second timeout
+
+        if ready_fds and ready_fds[1] then
+            for _, fd in ipairs(ready_fds[1]) do
+                local co = self.coroutines[fd]
+                if co and coroutine.status(co) == "suspended" then
+                    coroutine.resume(co)
+                end
+            end
+        end
+    end
+end
+
+return EventScheduler
